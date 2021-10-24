565e67b7361905fc1ab94ca42b18949220706647
edom18 <edo.m18@gmail.com>
2021-10-24 20:51:03 +0900

    feat: Added a control to move the point cloud.

--- Assets/Project/Scripts/PointCloudParticleView.cs
+++ Assets/Project/Scripts/PointCloudParticleView.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,22 +11,37 @@ public class PointCloudParticleView : MonoBehaviour
     [SerializeField] private Slider _slider;
 
     private bool _firstTake = true;
-    
+
+    [SerializeField] private Transform _container;
+    [SerializeField] private Button _posResetButton;
+    [SerializeField] private Button _rotResetButton;
     private bool _isDragging = false;
     private Vector3 _prevPos = Vector2.zero;
     private Vector3 _initPos = Vector3.zero;
+
+    private Vector3 _offsetPos = Vector3.zero;
 
     #region ### ------------------------------ MonoBehaviour ------------------------------ ###
 
     private void Awake()
     {
         _initPos = transform.localPosition;
-        
+
         UpdateParticle();
-        
+
         _slider.onValueChanged.AddListener((val) =>
         {
-            transform.localPosition = _initPos + new Vector3(0, 0, val);
+            _offsetPos.z = val;
+            UpdatePosition();
+        });
+
+        _rotResetButton.onClick.AddListener(() => { transform.localRotation = quaternion.identity; });
+
+        _posResetButton.onClick.AddListener(() =>
+        {
+            _offsetPos = Vector3.zero;
+            _slider.value = 0;
+            UpdatePosition();
         });
     }
 
@@ -33,6 +49,7 @@ public class PointCloudParticleView : MonoBehaviour
     {
         UpdateParticle();
 
+#if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
         {
             BeginTouch(Input.mousePosition);
@@ -40,37 +57,72 @@ public class PointCloudParticleView : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            Dragging(Input.mousePosition);
+            Dragging(Input.mousePosition, false);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             EndTouch();
         }
+#else
+        if (Input.touchCount == 0) return;
+
+        bool isRotate = Input.touchCount == 1;
+        
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            BeginTouch(touch.position);
+        }
+
+        if (touch.phase == TouchPhase.Moved)
+        {
+            Dragging(touch.position, isRotate);
+        }
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            EndTouch();
+        }
+#endif
     }
 
     #endregion ### ------------------------------ MonoBehaviour ------------------------------ ###
 
+    private void UpdatePosition()
+    {
+        transform.localPosition = _initPos + _offsetPos;
+    }
+
     private void BeginTouch(Vector3 startPos)
     {
         if (EventSystem.current.currentSelectedGameObject != null) return;
-        
+
         _isDragging = true;
         _prevPos = startPos;
     }
 
-    private void Dragging(Vector3 pos)
+    private void Dragging(Vector3 pos, bool isRotate)
     {
         if (!_isDragging) return;
 
         Vector3 delta = pos - _prevPos;
-
-        transform.Rotate(Vector3.up, delta.x);
-        transform.Rotate(Vector3.right, -delta.y);
-
         _prevPos = pos;
+
+        if (isRotate)
+        {
+            transform.Rotate(_container.up, delta.x, Space.World);
+            Vector3 right = Vector3.Cross(_container.forward, Vector3.up);
+            transform.Rotate(right, delta.y, Space.World);
+        }
+        else
+        {
+            _offsetPos += delta * 0.01f;
+            UpdatePosition();
+        }
     }
-    
+
     private void EndTouch()
     {
         _isDragging = false;
@@ -79,7 +131,7 @@ public class PointCloudParticleView : MonoBehaviour
     private void UpdateParticle()
     {
         if (!_pointCloudSource.IsReady) return;
-        
+
         Metadata metadata = _pointCloudSource.Metadata;
 
         if (_firstTake)
@@ -87,7 +139,7 @@ public class PointCloudParticleView : MonoBehaviour
             _firstTake = false;
             _pointCloudParticle.Initialize(metadata.depthResolution);
         }
-        
+
         _pointCloudParticle.ColorMap = _pointCloudSource.ColorTexture;
         _pointCloudParticle.DepthMap = _pointCloudSource.DepthTexture;
         _pointCloudParticle.Intrinsics = metadata.intrinsic;
